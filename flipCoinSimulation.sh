01fc871960b372c6ddbd290fb2a3d2df7fe533ae
gajanan15 <gajanandesai97@gmail.com>
2020-01-25 13:14:03 +0530

    ADDED: percentage calculation of doublet combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,24 +1,54 @@
-#!/bin/bash -x
+#!/bin/bash
+
+echo "Welcome to Flip coin Simulation"
+
 declare -A coin
 
-headCount=0
-tailCount=0
+function flipCoin()
+{
+   for((i=1;i<=$1;i++))
+   do
+      side=""
+      for((j=1;j<=$2;j++))
+      do
+         if [ $((RANDOM%2)) -eq 1 ]
+         then
+            side+=H
+         else
+            side+=T
+         fi
+      done
+      updateCount $side
+   done
+}
 
-read -p "How many times you want flip coin: " n
-for((i=1;i<=$n;i++))
-do
-   if [ $((RANDOM%2)) -eq 1 ]
-   then
-		coin[H]=$((++headCount))
-   else
-      coin[T]=$((++tailCount))
-   fi
-done
+function updateCount()
+{
+   coin[$1]=$((${coin[$1]}+1))
+}
 
-echo "Key   ${!coin[@]}"
-echo "Count ${coin[@]}"
+function calculatePercentage()
+{
+	for i in ${!coin[@]}
+	do
+		coin[$i]=`echo "scale=2; ${coin[$i]}*100/$noOfFlip" | bc`
+	done
+	echo "Key        ${!coin[@]}"
+	echo "Percentage ${coin[@]}"
+}
+read -p "How many times you want to flip a coin: " noOfFlip
+read -p "Enter your choice : 1) Singlet 2) Doublet " choice
+case $choice in
+   1)
+      noOfCoin=1
+      ;;
+   2)
+      noOfCoin=2
+      ;;
+   *)
+      echo "Invalid Choice"
+      ;;
+esac
 
-echo "Percentage of head: `echo "scale=2; ${coin[H]}*100/$n" | bc` %"
-echo "Percentage of tail: `echo "scale=2; ${coin[T]}*100/$n" | bc` %" 
-
-
+flipCoin $noOfFlip $noOfCoin
+calculatePercentage
